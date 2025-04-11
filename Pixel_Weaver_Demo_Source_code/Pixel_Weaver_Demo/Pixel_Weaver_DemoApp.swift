26c34db087614299c95181d00c9a5a8973378b22
Cong Le <cle155@csu.fullerton.edu>
2025-04-11 12:24:52 -0700

    initial version and set up

---
+++ Pixel_Weaver_Demo_Source_code/Pixel_Weaver_Demo/Pixel_Weaver_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_Weaver_DemoApp.swift
+//  Pixel_Weaver_Demo
+//
+//  Created by Cong Le on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_Weaver_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
